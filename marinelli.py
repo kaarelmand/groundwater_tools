74d6f2ae791e384c7b24b5df61b094437a65dacf
Kaarel Mänd <kaarel.mand@ut.ee>
2024-06-10 23:30:31 +0300

    fixed up reporting functions

--- marinelli.py
+++ marinelli.py
@@ -1,53 +1,82 @@
-import math
 import numpy as np
 from scipy import optimize
+import pandas as pd
+from functools import cached_property
+
+DAY_TO_SEC = 24 * 60 * 60
+YEAR_TO_SEC = 365.25 * 24 * 60 * 60
+M_TO_MM = 1000
 
 
 class PitFlow:
-    """All parameters are excpected to be in meters and seconds.
+    """All parameters are expected to be in meters and seconds.
     anisotropy defaults to 1 (i.e. sandstone), limestones can be 0.1. Only used for
         calculating bottom flow, in any case.
     h_edge=0 is conservative, hence default.
     """
 
+    _inputs_info = {
+        "drawdown_stab": ("Drawdown in pit center", "m"),
+        "drawdown_edge": ("Height of inflow at pit edge", "m"),
+        "cond_h": ("Hydraulic conductivity (horizontal)", "m/s"),
+        "anisotropy": ("Anisotropy of hydraulic conductivity", ""),
+        "recharge": ("Recharge", "m/s"),
+        "radius_eff": ("Effective radius of pit", "m"),
+        "depth_pitlake": ("Water depth in the pit lake", "m"),
+    }
+    _outputs_info = {
+        "radius_infl": ("Radius of influence from pit centre", "m"),
+        "radius_infl_from_edge": ("Radius of influence from pit edge", "m"),
+        "radius_at_1m": ("Radius at drawdown of 1 m, from pit edge", "m"),
+        "inflow_zone1": ("Zone 1 inflow", "m^3/s"),
+        "inflow_zone2": ("Zone 2 inflow", "m^3/s"),
+        "inflow_zones_both": ("Zone 1 and 2 inflow", "m^3/s"),
+    }
+
     def __init__(
         self,
         drawdown_stab,
-        trans_h,
+        cond_h,
         radius_eff,
         recharge,
         anisotropy=1,
         drawdown_edge=0,
+        depth_pitlake=0,
     ):
         self.drawdown_stab = drawdown_stab
-        self.trans_h = trans_h
+        self.cond_h = cond_h
         self.radius_eff = radius_eff
         self.recharge = recharge
         self.drawdown_edge = drawdown_edge
         self.anisotropy = anisotropy
-        self.radius_infl = self.get_depression_radius()
-        self.radius_infl_from_edge = self.radius_infl - self.radius_eff
+        self.depth_pitlake = depth_pitlake
 
-    def get_marinelli_niccoli_h_0(self, radius_infl):
+    def _get_marinelli_niccoli_h_0(self, radius_infl):
         """Marinelli and Niccoli 2000 formula describing horizontal groundwater flow
         into pit. radius_infl will be correct if return value is 0."""
         radius_infl = radius_infl.item()
         radius_term = (
-            radius_infl**2 * math.log(radius_infl / self.radius_eff)
+            radius_infl**2 * np.log(radius_infl / self.radius_eff)
             - (radius_infl**2 - self.radius_eff**2) / 2
         )
-        right_term = math.sqrt(
-            self.drawdown_edge**2 + (self.recharge / self.trans_h) * radius_term
+        right_term = np.sqrt(
+            self.drawdown_edge**2 + (self.recharge / self.cond_h) * radius_term
         )
         return self.drawdown_stab - right_term
 
-    def get_depression_radius(self, radius_start=10000):
-        """Find optimum depression radius through the Marinelli and Niccoli 2000
+    @cached_property
+    def radius_infl(self, radius_start=10000):
+        """Find optimum drawdown radius through the Marinelli and Niccoli 2000
         formula."""
-        return optimize.fsolve(func=self.get_marinelli_niccoli_h_0, x0=radius_start)[0]
+        return optimize.fsolve(func=self._get_marinelli_niccoli_h_0, x0=radius_start)[0]
 
-    def get_depression_at_r(self, radius_from_wall):
-        """Return depression at length `radius_from_wall` according to Marinelli &
+    @cached_property
+    def radius_infl_from_edge(self):
+        """Find optimum drawdown radius from the edge of the pit, not the center."""
+        return self.radius_infl - self.radius_eff
+
+    def get_drawdown_at_r(self, radius_from_wall):
+        """Return drawdown at length `radius_from_wall` according to Marinelli &
         Niccoli (2000)."""
         if radius_from_wall < 0:
             return self.drawdown_stab
@@ -56,26 +85,31 @@ class PitFlow:
         else:
             radius = radius_from_wall + self.radius_eff
             radius_term = (
-                self.radius_infl**2 * math.log(radius / self.radius_eff)
+                self.radius_infl**2 * np.log(radius / self.radius_eff)
                 - (radius**2 - self.radius_eff**2) / 2
             )
-            sqrt_term = math.sqrt(
-                self.drawdown_edge**2 + (self.recharge / self.trans_h) * radius_term
+            sqrt_term = np.sqrt(
+                self.drawdown_edge**2 + (self.recharge / self.cond_h) * radius_term
             )
             return self.drawdown_stab - sqrt_term
 
-    def balance_depression_threshold(self, radius_from_wall, threshold=1):
-        """Helper function to find radius where depression is at threshold."""
-        return self.get_depression_at_r(radius_from_wall[0]) - threshold
+    def _balance_drawdown_threshold(self, radius_from_wall, threshold=1):
+        """Helper function to find radius where drawdown is at threshold."""
+        return self.get_drawdown_at_r(radius_from_wall[0]) - threshold
 
-    def get_significant_radius(self, significant_threshold=1, x0=10):
-        """Return radius at which depression equals given threshold (default = 1 m).
+    def get_r_at_drawdown(self, drawdown, x0=10):
+        """Return radius at which drawdown equals given threshold.
         Unfortunately, has to be iterative as analytical solution is tough."""
         return optimize.fsolve(
-            func=self.balance_depression_threshold,
+            func=self._balance_drawdown_threshold,
             x0=x0,
-            args=(significant_threshold),
+            args=(drawdown),
         )[0]
+
+    @cached_property
+    def radius_at_1m(self):
+        """Radius from wall at which estimated water table drawdown is 1 m."""
+        return self.get_r_at_drawdown(1)
 
     def draw_drawdown_curve(self, ax, line_buffer=(0.15, 1.5), lims=None, **kwargs):
         """Draw the groundwater drawdown curve on an existing matplotlib axes.
@@ -98,79 +132,148 @@ class PitFlow:
                 self.radius_infl_from_edge * line_buffer[1],
             )
         radiuses = np.linspace(*lims, 1000)
-        drawdowns = [self.get_depression_at_r(r) for r in radiuses]
+        drawdowns = [self.get_drawdown_at_r(r) for r in radiuses]
         return ax.plot(radiuses, drawdowns, **kwargs)
 
-    # TODO: test
-    def get_zone1_inflow(self):
-        """Get horizontal inflow from zone 1, i.e. pit walls.
+    @cached_property
+    def inflow_zone1(self):
+        """Horizontal inflow into pit from zone 1, i.e. pit walls (m^3/s)."""
+        return self.recharge * np.pi * (self.radius_infl**2 - self.radius_eff**2)
 
-        Returns:
-            float: Inflow in m3/sec.
-        """
-        return self.recharge * math.pi * (self.radius_infl**2 - self.radius_eff**2)
-
-    # TODO: test
-    def get_zone2_inflow(self, pit_lake_depth=0):
-        """Get vertical inflow from zone 2, i.e. pit bottom.
-
-        Args:
-            pit_lake_depth (float, optional): Depth of the water column overlying the
-                pit bottom. Defaults to 0.
-
-        Returns:
-            float: Inflow in m3/sec.
-        """
-        anisotropy_term = math.sqrt(self.trans_h / (self.trans_h * self.anisotropy))
+    @cached_property
+    def inflow_zone2(self):
+        """Vertical inflow into pit from zone 2, i.e. pit bottom (m^3/sec)."""
+        anisotropy_term = np.sqrt(self.cond_h / (self.cond_h * self.anisotropy))
         return (
             4
             * self.radius_eff
-            * (self.trans_h / anisotropy_term)
-            * (self.drawdown_stab - pit_lake_depth)
+            * (self.cond_h / anisotropy_term)
+            * (self.drawdown_stab - self.depth_pitlake)
         )
+
+    @cached_property
+    def inflow_zones_both(self):
+        """Combined inflow into pit from zones 1 and 2, i.e. pit walls and bottom
+        (m^3/sec)."""
+        return self.inflow_zone1 + self.inflow_zone2
+
+    def report(self, drawdown_points=None):
+        """Convenient report of models inputs and commonly-needed model output values."""
+        report_table = (
+            pd.DataFrame(self._inputs_info | self._outputs_info)
+            .T.reset_index()[[0, "index", 1]]
+            .rename(columns={0: "Parameter", 1: "Unit", "index": "Value"})
+        )
+        report_table["Value"] = report_table["Value"].apply(lambda x: getattr(self, x))
+
+        if not drawdown_points:
+            drawdown_points = get_nice_intervals(self.radius_infl_from_edge)
+        drawdown_rows = pd.DataFrame(
+            (
+                [
+                    f"Drawdown at {point} m from pit wall",
+                    self.get_drawdown_at_r(point),
+                    "m",
+                ]
+                for point in drawdown_points
+            ),
+            columns=["Parameter", "Value", "Unit"],
+        )
+        report_table = pd.concat([report_table, drawdown_rows])
+
+        return report_table
+
+    def __repr__(self):
+        fmt_output = f"{self.__class__.__name__}("
+        for param in self._inputs_info.keys():
+            fmt_output += f"\n{param}={getattr(self, param)},"
+        return fmt_output + "\n)"
+
+    def __str__(self):
+        fmt_output = f"{self.__class__.__name__}:"
+        for param, (name, unit) in self._inputs_info.items():
+            fmt_output += f"\n{name}: {getattr(self, param)} {unit}"
+        return fmt_output
+
+    def _repr_html_(self):
+        fmt_output = (
+            '<table border="1">'
+            f'<tr><th colspan="3">{self.__class__.__name__}</th></tr>'
+        )
+        for param, (name, unit) in self._inputs_info.items():
+            fmt_output += (
+                f"\n<tr><td>{name}</td>"
+                f"<td>{getattr(self, param):.6f}</td>"
+                f"<td>{unit}</td></tr>"
+            )
+        return fmt_output + "</table>"
 
 
 class PitFlowCommonUnits(PitFlow):
     """Same as PitFlow but expects input in more convenient forms:
-    `trans_h_md` (horizontal transmissivity) in m/d
+    `cond_h_md` (horizontal hydraulic conductivity) in m/d
     `area` (total pit area) in m^2
-    `precipitation` in mm/yr
-    `infil_coef=0.1` is a typical value for most of Estonia.
+    `recharge_mm_yr` in mm/yr
     """
+
+    _inputs_info = {
+        "cond_h_md": ("Hydraulic conductivity (horizontal)", "m/d"),
+        "recharge_mm_yr": ("Recharge", "mm/yr"),
+        "area": ("Total pit area", "m^2"),
+    } | PitFlow._inputs_info
 
     def __init__(
         self,
-        drawdown_stab,
-        trans_h_md,
-        anisotropy,
+        cond_h_md,
+        recharge_mm_yr,
         area,
-        precip,
+        drawdown_stab,
+        anisotropy=1,
         drawdown_edge=0,
-        infil_coef=0.1,
+        depth_pitlake=0,
     ):
-        self.trans_h_md = trans_h_md
+        self.cond_h_md = cond_h_md
+        self.recharge_mm_yr = recharge_mm_yr
         self.area = area
-        self.precip = precip
-        self.infil_coef = infil_coef
         super().__init__(
             drawdown_stab=drawdown_stab,
-            trans_h=self.transmissivity_to_m_sec(),
+            cond_h=cond_h_md / DAY_TO_SEC,
+            radius_eff=get_effective_radius(area),
+            recharge=recharge_mm_yr / YEAR_TO_SEC / M_TO_MM,
             anisotropy=anisotropy,
-            radius_eff=self.get_effective_radius(),
-            recharge=self.get_recharge(),
             drawdown_edge=drawdown_edge,
+            depth_pitlake=depth_pitlake,
         )
 
-    def transmissivity_to_m_sec(self):
-        """Unit conversion of transmissivity from m/d to m/s."""
-        return self.trans_h_md / (24 * 60 * 60)
+    def __repr__(self):
+        fmt_output = f"{self.__class__.__name__}("
+        for param in self._inputs_info.keys():
+            if param not in ["recharge", "cond_h", "radius_eff"]:
+                fmt_output += f"\n{param}={getattr(self, param)},"
+        return fmt_output + "\n)"
 
-    def get_effective_radius(self):
-        """Return ideal circularized radius from true quarry area."""
-        return math.sqrt(self.area / math.pi)
 
-    def get_recharge(self):
-        """Return recharge in m/sec, input needs to be in mm/yr. infil_coef=0.1 is
-        typical for Estonia."""
-        precip_m_sec = self.precip / (1000 * 365.25 * 24 * 60 * 60)
-        return precip_m_sec * self.infil_coef
+def get_effective_radius(area):
+    """Return ideal circularized radius from true area."""
+    return np.sqrt(area / np.pi)
+
+
+def get_nice_intervals(end, num_bounds=(4, 8)):
+    """Return rounded intervals from 0 to `end`."""
+    mag = np.floor(np.log10(end))
+    digit1 = end // 10**mag % 10
+    digit2 = end // 10 ** (mag - 1) % 10
+
+    if digit2 >= 5:
+        max = (digit1 + 1) * 10**mag
+    elif digit2 < 5:
+        max = (digit1 + 0.5) * 10**mag
+
+    num = int(max / (0.5 * 10**mag))
+    if num > num_bounds[1]:
+        if digit2 < 5:
+            max += 0.5 * 10**mag
+        num = int(np.ceil(num / 2))
+    elif num < num_bounds[0]:
+        num *= 2
+    return np.linspace(0, max, num + 1)[1:]
--- test_marinelli.py
+++ test_marinelli.py
@@ -1,41 +1,65 @@
 import pytest
+import numpy as np
 import math
 
-from marinelli import PitFlow, PitFlowCommonUnits
+from marinelli import PitFlow, PitFlowCommonUnits, get_nice_intervals
 
 testpit = PitFlow(
     drawdown_stab=6,
-    trans_h=20 / (24 * 60 * 60),
-    radius_eff=math.sqrt(40 * 100 / math.pi),
+    cond_h=20 / (24 * 60 * 60),
+    radius_eff=np.sqrt(40 * 100 / np.pi),
     recharge=761 / (1000 * 365.25 * 24 * 60 * 60) * 0.1,
 )
 
+testpit_anisotropic = PitFlow(
+    drawdown_stab=6,
+    cond_h=20 / (24 * 60 * 60),
+    radius_eff=np.sqrt(40 * 100 / np.pi),
+    recharge=761 / (1000 * 365.25 * 24 * 60 * 60) * 0.1,
+    anisotropy=0.1,
+)
 
-def test_get_depression_radius():
+testpit_commonunits = PitFlowCommonUnits(
+    drawdown_stab=6,
+    cond_h_md=20,
+    area=40 * 100,
+    recharge_mm_yr=761 * 0.1,
+)
+
+
+def test_radius_infl():
     assert math.isclose(testpit.radius_infl, 1088.212649, rel_tol=1e-6)
 
 
-def test_get_depression_at_r_100():
-    assert math.isclose(testpit.get_depression_at_r(100), 1.951781, rel_tol=1e-6)
+def test_get_drawdown_at_r_100():
+    assert math.isclose(testpit.get_drawdown_at_r(100), 1.951781, rel_tol=1e-6)
 
 
-def test_get_depression_at_r_neg20():
-    assert testpit.get_depression_at_r(-20) == 6
+def test_get_drawdown_at_r_neg20():
+    assert testpit.get_drawdown_at_r(-20) == 6
 
 
-def test_get_depression_at_r_1100():
-    assert testpit.get_depression_at_r(1100) == 0
+def test_get_drawdown_at_r_1100():
+    assert testpit.get_drawdown_at_r(1100) == 0
 
 
-def test_get_significant_radius():
-    assert math.isclose(testpit.get_significant_radius(), 244.000377, rel_tol=1e-6)
+def test_get_r_at_drawdown():
+    assert math.isclose(testpit.radius_at_1m, 244.000377, rel_tol=1e-6)
 
 
-def test_get_depression_radius_commonunits():
-    testpit = PitFlowCommonUnits(
-        drawdown_stab=6,
-        trans_h_md=20,
-        area=40 * 100,
-        precip=761,
-    )
-    assert math.isclose(testpit.radius_infl, 1088.212649)
+def test_inflow_zone1():
+    assert math.isclose(testpit.inflow_zone1, 0.008962, rel_tol=1e-4)
+
+
+def test_inflow_zone2():
+    assert math.isclose(testpit_anisotropic.inflow_zone2, 0.062688, rel_tol=1e-5)
+
+
+def test_radius_infl_commonunits():
+    assert math.isclose(testpit_commonunits.radius_infl, 1088.212649)
+
+
+def test_get_nice_intervals():
+    assert (
+        get_nice_intervals(174.10374226797754) == np.array([50.0, 100.0, 150.0, 200.0])
+    ).all()
